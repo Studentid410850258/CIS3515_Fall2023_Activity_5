5e96be592230117c194f9ece7256952e763eeb3e
Studentid410850258 <134401041+Studentid410850258@users.noreply.github.com>
2023-09-29 00:17:20 +0800

    completed.

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -1,10 +1,9 @@
-package edu.temple.simplerecyclerview
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import edu.temple.simplerecyclerview.NumberDisplayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,15 +11,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Set up RecyclerView display responsibility
-        val numbers = Array(100) {it + 1}
+        val numbers = Array(100) { it + 1 }
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         val displayTextView = findViewById<TextView>(R.id.displayTextView)
         recyclerView.layoutManager = GridLayoutManager(this, 4)
 
-        // TODO (Step 2: Define lambda to modify displayTextView size)
+        // Define a lambda to modify displayTextView size
+        val modifyDisplayText: (Int) -> Unit = { clickedNumber ->
+            displayTextView.text = "Clicked number: $clickedNumber"
+        }
 
-        // Todo (Step 3: Pass lambda to adapter)
-        recyclerView.adapter = NumberDisplayAdapter(numbers)
-
+        // Pass the lambda to the adapter
+        recyclerView.adapter = NumberDisplayAdapter(numbers, modifyDisplayText)
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,25 +1,29 @@
-package edu.temple.simplerecyclerview
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO(Step 1: Implement click behavior)
+class NumberDisplayAdapter(
+    private val numbers: Array<Int>,
+    private val onItemClick: (Int) -> Unit
+) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-// TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() /* Step 3a: Provide Adapter Parent */ {
-
-    // TODO (Step 1b: Invoke lambda via onClickListener)
-    inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
-        // enumerate views inside layout
+    inner class NumberViewHolder(layout: View) : RecyclerView.ViewHolder(layout) {
         val textView = layout.findViewById<TextView>(R.id.textView)
+
+        init {
+            textView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onItemClick(numbers[position])
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
         return NumberViewHolder(
-            // Inflate prebuilt layout file
             LayoutInflater.from(parent.context).inflate(R.layout.textview_layout, parent, false)
         )
     }
@@ -31,5 +35,4 @@ class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adap
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
         holder.textView.text = numbers[position].toString()
     }
-
 }
